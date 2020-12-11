ed20b389bbd51cc5df64574f8ab6a4449b636c4b
ANZania <zianastasia@yandex.ru>
2020-12-11 16:48:22 +0300

    Add items database & App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { NavBar } from './Components/NavBar';
+import { Promo } from './Components/Promo';
+import { Menu } from './Components/Menu';
+import { Footer } from './Components/Footer';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    background-color: #CACACA;
+    font-family: Roboto, sans-serif;
+    font-size: 20px;
+    color: black
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  a {
+    next-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  h1, h2, h3, p {
+    padding: 0;
+    margin: 0;
+
+  }
+
+  .promoImg {
+    width: 100%;
+    height: auto;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle/>
+      <NavBar/>
+      <Promo/>
+      <Menu/>
+      <Footer/>
+    </>
   );
-}
+};
 
-export default App;
+export default App
