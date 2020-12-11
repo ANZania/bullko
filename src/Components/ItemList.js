8d82b5521e3519097de1f6279b71f31c6f6563ff
ANZania <zianastasia@yandex.ru>
2020-12-12 00:22:09 +0300

    Add menu & items components base

---
+++ src/Components/ItemList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+// STYLES 
+
+
+const ItemListStyled = styled.div``;
+
+
+// COMPONENT
+
+
+export const ItemList = () => (
+    <ItemListStyled>
+
+    </ItemListStyled>
+);
+
---
+++ src/Components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import ItemList from './ItemList';
+
+
+// STYLES 
+
+const MenuStyled = styled.div``;
+
+
+// COMPONENT
+
+
+export const Menu = () => (
+    <MenuStyled>
+        <ItemList/>
+    </MenuStyled>
+);
