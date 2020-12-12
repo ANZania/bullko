fb7920deabab774cb0a431a71a8bac2e9817cd23
ANZania <zianastasia@yandex.ru>
2020-12-12 13:34:08 +0300

    Create items cards

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -5,15 +5,61 @@ import styled from 'styled-components';
 // STYLES 
 
 
-const ItemListStyled = styled.div``;
+const ItemListStyled = styled.ul`
+    padding-top: 26px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`;
+
+const Item = styled.li`
+    margin-top: 25px;
+    width: 32%;
+    height: 190px;
+    transition: 0.5s;
+    background-size: cover;
+    background-image: ${(props) => `url(${props.img})`};
+    filter: brightness(0.7);
+    border-radius: 5px;
+    &:hover {
+        filter: brightness(1);
+        transition-duration: 0.5s;
+        transform: scale(0.9, 0.9);
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
+    }
+`;
+
+const ItemName = styled.p`
+    color: #ffffff;
+    font-weight: 400;
+    font-size: 16;
+    padding-top: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+`;
+
+const ItemPrice = styled.p`
+    color: #ffffff;
+    font-weight: 100;
+    font-size: 16;
+    padding-top: 5px;
+    padding-left: 10px;
+    padding-right: 10px;    
+`;
 
 
 // COMPONENT
 
-const ItemList = () => (
+export const ItemList = ({ itemList }) => (
     <ItemListStyled>
+        {itemList.map( item => (
+            <Item 
+                key={item.id}
+                img={item.img}>
+                <ItemName>{item.name}</ItemName>
+                <ItemPrice>{item.price.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'})}</ItemPrice>
 
+            </Item>
+        ))}
     </ItemListStyled>
-);
-
-export default ItemList;
+)
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import ItemList from './ItemList';
+import DBMenu from './DB';
+import { ItemList } from './ItemList';
 
 
 // STYLES 
 
-const MenuStyled = styled.div``;
+const MenuStyled = styled.main`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 100%;
+    background-color: #ffffff;
+`;
+
+const ItemBlock = styled.div`
+    width: 70%;
+`;
+
+const ItemTitle = styled.h3`
+    padding-top: 3%;
+    display: flex;
+    justify-content: flex-start;
+    color: #000000;
+    font-weight: 400;
+    font-size: 30px;
+`;
 
 
 // COMPONENT
@@ -13,6 +33,13 @@ const MenuStyled = styled.div``;
 
 export const Menu = () => (
     <MenuStyled>
-        <ItemList/>
+        <ItemBlock>
+            <ItemTitle>Бургеры</ItemTitle>
+            <ItemList itemList={DBMenu.burger} />
+            <ItemTitle>Напитки</ItemTitle>
+            <ItemList itemList={DBMenu.drinks} />
+            <ItemTitle>Закуски</ItemTitle>
+            <ItemList itemList={DBMenu.snacks} />
+        </ItemBlock>
     </MenuStyled>
 );
